62783a94e0ceccb92e8ecd36f1bff707ebd35817
JUNGHOONHAM <3hampsen@gamil.com>
2020-01-27 16:48:25 +0000

    json 데이터 내림차순

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -9,67 +9,67 @@ class HomesController < ApplicationController
   
   ## 내 DB에 저장된 데이터를 Json으로 출력
   def youtube_result
-    @youtube = Youtube.all.to_json(:except => [:id, :created_at, :updated_at])
+    @youtube = Youtube.all.order(publishedAt: :desc).to_json(:except => [:id, :created_at, :updated_at])
 
     render :json => @youtube
   end
   
   def youtube_ytn
-    @youtube = Youtube.where(channelTitle: "UChlgI3UHCOnwUGzWzbJ3H5w").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UChlgI3UHCOnwUGzWzbJ3H5w").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_jtbc
-    @youtube = Youtube.where(channelTitle: "UCsU-I-vHLiaMfV_ceaYz5rQ").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCsU-I-vHLiaMfV_ceaYz5rQ").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_channelA
-    @youtube = Youtube.where(channelTitle: "UCfq4V1DAuaojnr2ryvWNysw").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCfq4V1DAuaojnr2ryvWNysw").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_mbn
-    @youtube = Youtube.where(channelTitle: "UCG9aFJTZ-lMCHAiO1KJsirg").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCG9aFJTZ-lMCHAiO1KJsirg").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_sbs
-    @youtube = Youtube.where(channelTitle: "UCkinYTS9IHqOEwR1Sze2JTw").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCkinYTS9IHqOEwR1Sze2JTw").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_kbs
-    @youtube = Youtube.where(channelTitle: "UCcQTRi69dsVYHN3exePtZ1A").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCcQTRi69dsVYHN3exePtZ1A").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_mbc
-    @youtube = Youtube.where(channelTitle: "UCF4Wxdo3inmxP-Y59wXDsFw").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCF4Wxdo3inmxP-Y59wXDsFw").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_tvChoseon
-    @youtube = Youtube.where(channelTitle: "UCuw1hxBo5mDVUhgMzRDk3aw").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCuw1hxBo5mDVUhgMzRDk3aw").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_video
-    @youtube = Youtube.where(channelTitle: "UCMEbRpvuwTbXxGiyDb1mT8w").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCMEbRpvuwTbXxGiyDb1mT8w").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
   
   def youtube_sebese
-    @youtube = Youtube.where(channelTitle: "UCQabORQKQRHP-iUqe-xIFvg").to_json(:except => [:id])
+    @youtube = Youtube.where(channelTitle: "UCQabORQKQRHP-iUqe-xIFvg").order(publishedAt: :desc).to_json(:except => [:id])
 
     render :json => @youtube
   end
