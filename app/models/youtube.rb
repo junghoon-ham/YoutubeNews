04b5d326f43402f6d659c95d76745fee4c7ed498
JUNGHOONHAM <3hampsen@gamil.com>
2020-02-27 08:16:58 +0000

    코드 리팩토링

--- app/models/youtube.rb
+++ app/models/youtube.rb
@@ -1,3 +1,13 @@
 class Youtube < ApplicationRecord
     validates_uniqueness_of :title
+    
+    def self.channel_list_without_drop(channel_title, current_time, size)
+        result = Youtube.where(channelTitle: channel_title).where("created_at <= :currnetTime AND live = :live", :currnetTime => current_time, :live => 'none').order(publishedAt: :desc).uniq.first(size)
+        return result
+    end
+    
+    def self.channel_list(channel_title, current_time, startNumber, size)
+        result = Youtube.where(channelTitle: channel_title).where("created_at <= :currnetTime AND live = :live", :currnetTime => current_time, :live => 'none').order(publishedAt: :desc).uniq.drop(startNumber).first(size)
+        return result
+    end
 end
